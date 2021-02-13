b319a197f72bb798a5f04197550e1b1db91c0641
admin-PC\admin <franceatmaca@gmail.com>
2021-02-13 21:41:09 +0300

    Settings object

---
+++ Assets/Script/Scriptable/BallSettings.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+[CreateAssetMenu(menuName ="Settings/Ball Setting")]
+public class BallSettings : ScriptableObject
+{
+    
+    [Range(5, 20)]
+    [SerializeField] private float _sensivityDrag = 10;
+    [Range(10, 200)]
+    [SerializeField] private float _powerMultiplier = 10;
+
+    public float Sensivity { get { return _sensivityDrag; } }
+    public float Power { get { return _powerMultiplier; } }
+}
---
+++ Assets/Script/Scriptable/LevelSetting.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Settings/Level Setting")]
+public class LevelSetting : ScriptableObject
+{
+    [Header("Size")]
+    [SerializeField] private int _towerSize = 10;
+    [SerializeField] private int _blockSize = 30;
+
+    [Header("Position")]
+    [Range(0, 255)]
+    [SerializeField] private int _min = 15;
+    [SerializeField] private int _lastBlock = 85;
+    [SerializeField] private int _seperateTowerParts = 2;
+    [Header("Position to Last Block")]
+    [SerializeField] private int _finishLine = 10;
+    [SerializeField] private int _pointsBlock = 15;
+ 
+
+
+    public int TowerSize { get { return _towerSize; } set { _towerSize = value; } }
+    public int BlockSize { get { return _blockSize; } set { _blockSize = value; } }
+    public int MinPos { get { return _min; } set { _min = value; } }
+    public int Seperate { get { return _seperateTowerParts; } set { _seperateTowerParts = value; } }
+    public int LastBlock { get { return _lastBlock; } set { _lastBlock = value; } }
+    public int FinishLine { get { return _finishLine; } set { _finishLine = value; } }
+    public int PointBlock { get { return _pointsBlock; } set { _pointsBlock = value; } }
+
+
+}
+
